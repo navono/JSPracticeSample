42c46967cbe9c4d60ce130cc5e1393cec813e6d8
navono007 <navono007@gmail.com>
2017-06-02 21:17:41 +0800

    Add some shortcut key

---
+++ demo.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: Ping Qixing 
+ * @Date: 2017-06-02 20:59:16 
+ * @Last Modified by: Ping Qixing
+ * @Last Modified time: 2017-06-02 21:15:50
+ * @ Ctrl + Alt + i
+ */
+
+
+/**
+ * Ctrl + Alt + D
+ * 
+ * @returns 
+ */
+function getUserName(local) {
+    if (local == true) {
+        return "Pint"
+    }
+ 
+    return "Qixing";
+}
+
+
+/**
+ * Shift + Alt + U 上传VSCode的配置信息到Github
+ * Shift + Alt + D 下载VSCode的配置
+ * 
+ * Ctrl + K + 0 收起代码块
+ * Ctrl + K + j 展开代码块
+ * 
+ */
+let a = '1';
