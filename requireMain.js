2bbe9161dbcefc44966263c79b8b6cbbad42a9a4
pingqixing <pingqixing@supcon.com>
2017-06-07 10:56:43 +0800

    增加require.js的练习代码

---
+++ requireMain.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Ping Qixing
+ * @Date: 2017-06-07 10:36:17
+ * @Last Modified by: Ping Qixing
+ * @Last Modified time: 2017-06-07 10:37:37
+ */
+require.config({
+    paths: {
+        // 'jquery': ['http://libs.baidu.com/jquery/2.0.3/jquery'],
+        'demo': './requireDemo'
+    }
+})
