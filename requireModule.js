323fc6db62e00d3151e4ddb687b076faba5256af
pingqixing <pingqixing@supcon.com>
2017-06-07 14:18:10 +0800

    增加模块define的写法

--- requireModule.js
+++ requireModule.js
@@ -2,7 +2,7 @@
  * @Author: Ping Qixing
  * @Date: 2017-06-07 10:06:41
  * @Last Modified by: Ping Qixing
- * @Last Modified time: 2017-06-07 13:15:34
+ * @Last Modified time: 2017-06-07 14:17:25
  */
 
 // Normal
@@ -27,18 +27,28 @@
 
 // define用来定义个模块
 // require用来加载依赖模块，在页面中使用
-define(function () {
-    'use strict';
-    // function func1 () {
-    //     alert('it works');
-    // }
 
-    // func1();
-    // 或者
+// define(function () {
+//     'use strict';
+//     // function func1 () {
+//     //     alert('it works');
+//     // }
 
-    return {
-        hello: function () {
+//     // func1();
+//     // 或者
+
+//     return {
+//         hello: function () {
+//             alert('it works');
+//         }
+//     }
+// });
+
+// or
+define(
+    function (require, exports, module) {
+        exports.hello = function () {
             alert('it works');
         }
     }
-});
+);
