095ab1c7c1ea06e3128d119cc4b6d9aad07daeb5
navono007 <navono007@gmail.com>
2017-06-11 21:29:25 +0800

    增加快捷键

--- shortcutKeyCommand.js
+++ shortcutKeyCommand.js
@@ -3,7 +3,7 @@
  * @Author: Ping Qixing
  * @Date: 2017-06-02 20:59:16
  * @Last Modified by: Ping Qixing
- * @Last Modified time: 2017-06-05 11:00:19
+ * @Last Modified time: 2017-06-11 21:29:15
  *
  * Ctrl + Alt + i
  */
@@ -33,4 +33,10 @@ function getUserName (local) {
  * Ctrl + K + 0 收起代码块
  * Ctrl + K + j 展开代码块
  *
+ * Ctrl + b     收起左侧栏
+ *
+ * Ctrl + Shift + E     展开左侧的`文件列表`界面
+ * Ctrl + Shift + G     展开左侧的`源代码控制`界面
+ * Ctrl + Shift + D     展开左侧的`调试`界面
+ * Ctrl + Shift + F     展开左侧的`搜索`界面
  */
