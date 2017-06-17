fd547ec92a2ea5cd2a49e041ff8a022851013d03
navono007 <navono007@gmail.com>
2017-06-17 15:11:11 +0800

    增加尾递归和尾递归优化的示例代码

--- basicSyntaxSample/es6Syntax.js
+++ basicSyntaxSample/es6Syntax.js
@@ -2,10 +2,16 @@
  * @Author: Ping Qixing
  * @Date: 2017-06-11 10:46:56
  * @Last Modified by: Ping Qixing
- * @Last Modified time: 2017-06-14 21:18:39
+ * @Last Modified time: 2017-06-17 15:10:49
  */
 
 // Arrow function and template string
+// 箭头函数有几个使用注意点。
+// （1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
+// （2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误。
+// （3）不可以使用arguments对象，该对象在函数体内不存在。如果要用，可以用 rest 参数代替。
+// （4）不可以使用yield命令，因此箭头函数不能用作 Generator 函数。
+
 let loardify = firstname => `${firstname} of Canterbury`;
 // test code
 // console.log(loardify('Ping', 'Qixing'));
@@ -254,8 +260,8 @@ class Expedition extends Vacation {
 }
 
 const trip = new Expedition('Mt. Whitney', 3, ['sunglasses', 'prayer flags', 'camera']);
-trip.print();
-console.log(Vacation.prototype);
+// trip.print();
+// console.log(Vacation.prototype);
 
 // immutable
 let colorLawn = {
@@ -303,5 +309,110 @@ const pureSelfEducate = person => {
 //     canWrite: true
 // })
 
-console.log(pureSelfEducate(frederick));
-console.log(frederick);
+// console.log(pureSelfEducate(frederick));
+// console.log(frederick);
+
+function throwIfMissing () {
+    throw new Error('Missing parameter');
+}
+
+function foo (mustBeProvided = throwIfMissing()) {
+    return mustBeProvided;
+}
+
+// foo();
+
+// pipeline
+const pipeline = (...funcs) =>
+  val => funcs.reduce((a, b) => b(a), val);
+
+const plus1 = a => a + 1;
+const mult2 = a => a * 2;
+const addThenMult = pipeline(plus1, mult2);
+
+addThenMult(5)
+
+// equal
+mult2(plus1(5))
+
+// 绑定this
+// let log = ::console.log;
+let log = console.log.bind(console);
+
+// 非尾递归
+function Fibonacci (n) {
+    if (n <= 1) { return 1 };
+
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+// Fibonacci(10) // 89
+// Fibonacci(100) // 堆栈溢出
+// Fibonacci(500) // 堆栈溢出
+
+// 尾递归
+function Fibonacci2 (n, ac1 = 1, ac2 = 1) {
+    if (n <= 1) { return ac2 };
+
+    return Fibonacci2(n - 1, ac2, ac1 + ac2);
+}
+
+// Fibonacci2(100) // 573147844013817200000
+// Fibonacci2(1000) // 7.0330367711422765e+208
+// Fibonacci2(10000) // Infinity
+
+function factorial (n) {
+    if (n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+factorial(5) // 120
+
+function factorialTail (n, total) {
+    if (n === 1) return total;
+    return factorialTail(n - 1, n * total);
+}
+
+factorial(5, 1) // 120
+
+// 尾递归的实现，往往需要改写递归函数，确保最后一步只调用自身。
+// 做到这一点的方法，就是把所有用到的内部变量改写成函数的参数。
+
+// 比如上面的例子，阶乘函数 factorial 需要用到一个中间变量total，那就把这个中间变量改写成函数的参数。
+// 这样做的缺点就是不太直观，第一眼很难看出来，为什么计算5的阶乘，需要传入两个参数5和1？
+
+// 两个方法可以解决这个问题。方法一是在尾递归函数之外，再提供一个正常形式的函数。
+function tailFactorial (n, total) {
+    if (n === 1) return total;
+    return tailFactorial(n - 1, n * total);
+}
+
+function factorial2 (n) {
+    return tailFactorial(n, 1);
+}
+
+// 函数式编程有一个概念，叫做柯里化（currying），意思是将多参数的函数转换成单参数的形式。这里也可以使用柯里化。
+function currying (fn, n) {
+    return function (m) {
+        return fn.call(this, m, n);
+    };
+}
+
+function tailFactorial2 (n, total) {
+    if (n === 1) return total;
+    return tailFactorial2(n - 1, n * total);
+}
+
+const factorial3 = currying(tailFactorial2, 1);
+
+factorial3(5) // 120
+
+// 第二种方法就简单多了，就是采用 ES6 的函数默认值。
+function factorial4 (n, total = 1) {
+    if (n === 1) return total;
+    return factorial(n - 1, n * total);
+}
+
+factorial4(5) // 120
+
+// ES6 的尾调用优化只在严格模式下开启，正常模式是无效的。
