be509e8483775be73eaa9357b7fe16c4f24b2c4e
navono <navono007@gmail.com>
2017-07-12 15:18:07 +0800

    重新组织react组件的层级结构

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const PORT = 9999;
 
 module.exports = {
   devtool: 'source-map',
-  entry: path.resolve(__dirname, './react/index.js'),
+  entry: path.resolve(__dirname, './react/components/recipes/index.js'),
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, './build')
