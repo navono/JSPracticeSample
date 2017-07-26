22294c0e47e84224d8f8404e6a9765fd295eaf95
unknown <pingqixing@rd.supcon.com>
2017-07-26 09:49:42 +0800

    修改输出的JSON的字段名

--- basicSyntaxSample/normalizingJSON.js
+++ basicSyntaxSample/normalizingJSON.js
@@ -2,13 +2,13 @@ import { normalize, schema } from 'normalizr';
 import data from './sampleData.json';
 
 // Define a usres schema
-const author = new schema.Entity('author');
+const users = new schema.Entity('users');
 
 // Define your comments schema
 const comment = new schema.Entity(
   'comment',
   {
-    commenter: author
+    commenter: users
   }
 );
 
@@ -16,10 +16,31 @@ const comment = new schema.Entity(
 const article = new schema.Entity(
   'articles',
   {
-    author: author,
+    author: users,
     comments: [ comment ]
   }
 );
 
 const normalizedData = normalize(data, article);
 console.log(normalizedData);
+
+// {
+//   result: "123",
+//   entities: {
+//     "articles": { 
+//       "123": { 
+//         id: "123",
+//         author: "1",
+//         title: "My awesome blog post",
+//         comments: [ "324" ]
+//       }
+//     },
+//     "users": {
+//       "1": { "id": "1", "name": "Paul" },
+//       "2": { "id": "2", "name": "Nicole" }
+//     },
+//     "comments": {
+//       "324": { id: "324", "commenter": "2" }
+//     }
+//   }
+// }
