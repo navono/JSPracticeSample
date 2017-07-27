cd2b9d3f8ca7268f7a61ed797e09a76bbc1b1c6f
unknown <pingqixing@rd.supcon.com>
2017-07-27 19:43:30 +0800

    增加echarts代碼

---
+++ echarts/echartsDemo.js
@@ -0,0 +1,23 @@
+import echarts from 'echarts';
+
+let myChart = echarts.init(document.getElementById('root'));
+let option = {
+  title: {
+    text: 'Echarts'
+  },
+  tooltip: {},
+  legend: {
+    data:['销量']
+  },
+  xAxis: {
+    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+  },
+  yAxis: {},
+  series: [{
+    name: '销量',
+    type: 'bar',
+    data: [5, 20, 36, 10, 10, 20]
+  }]
+};
+
+myChart.setOption(option);
