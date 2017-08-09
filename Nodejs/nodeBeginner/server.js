f1dfd7d83e5634ee66eef9b1c44deec1351624d3
navono <navono007@gmail.com>
2017-08-09 09:05:02 +0800

    之前采用的是将“内容”返回给服务器，服务器用来response；目前为了支持非阻塞，将服务器的response传递给handler，由handler决定何时调用response。

--- Nodejs/nodeBeginner/server.js
+++ Nodejs/nodeBeginner/server.js
@@ -6,10 +6,7 @@ function start(route, handle) {
     const pathname = url.parse(req.url).pathname;
     console.log(`Request for ${pathname} received`);
     
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    const content = route(handle, pathname);
-    res.write(content);
-    res.end();
+    route(handle, pathname, res);
   }).listen(8989);
 
   console.log('Server has started.');
