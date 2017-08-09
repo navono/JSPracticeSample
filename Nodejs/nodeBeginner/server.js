561bc509e5f75f2a05985aaf514e74377af0941c
navono <navono007@gmail.com>
2017-08-09 09:34:07 +0800

    处理POST消息

--- Nodejs/nodeBeginner/server.js
+++ Nodejs/nodeBeginner/server.js
@@ -6,7 +6,15 @@ function start(route, handle) {
     const pathname = url.parse(req.url).pathname;
     console.log(`Request for ${pathname} received`);
     
-    route(handle, pathname, res);
+    let postData = '';
+    req.setEncoding('utf8');
+    req.addListener('data', dataChunk => {
+      postData += dataChunk;
+      console.log(`Received POST data chunk: ${dataChunk}`);
+    });
+    req.addListener('end', () => {
+      route(handle, pathname, res, postData);
+    });
   }).listen(8989);
 
   console.log('Server has started.');
