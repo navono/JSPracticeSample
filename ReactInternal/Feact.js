d5b323cf8ecad16dc2d495d7f1721cb616994d62
navono <navono007@gmail.com>
2017-08-21 08:57:08 +0800

    增加注释。在自定义的class里面可以返回子组件

--- ReactInternal/Feact.js
+++ ReactInternal/Feact.js
@@ -25,7 +25,12 @@ class FeactCompositeComponentWrapper {
   mountComponent(container) {
     const Component = this._currentElement.type;
     const componentInstance = new Component(this._currentElement.props);
-    const element = componentInstance.render();
+    let element = componentInstance.render();
+
+    // 这里先暂时使用循环来检查 element 的类型
+    while (typeof element.type === 'function') {
+      element = (new element.type(element.props)).render();
+    }
 
     const domComponentInstance = new FeactDOMComponent(element);
     return domComponentInstance.mountComponent(container);
@@ -60,20 +65,36 @@ const Feact = {
   //   const componentInstance = new FeactDOMComponent(element);
   //   return componentInstance.mountComponent(container);
   // }
+
   render(element, container) {
+    // 此处应该判断 element 是 原生的DOM元素还是 组合组件。
+    // 如果是 原生的DOM元素，可以用上面的 render 方法；
+    // 如果是 组合组件，就用下面的这个辅助类
     console.log('Composite');
     const componentInstance = new FeactCompositeComponentWrapper(element);
     return componentInstance.mountComponent(container);
   }
 };
 
+// 在创建自定义的 组件类 时，需要预先定义好这个 组件类 中的元素，
+// 也就是自定义 render 方法。在 render 方法里创建 元素
 const MyTitle = Feact.createClass({
   render() {
-    return Feact.createElement('h1', null, this.props.msg);
+    // 此时这里如果返回一个 组合组件，那么 FeactCompositeComponentWrapper 里
+    // 需要相应修改
+    if (this.props.asTitle) {
+      return Feact.createElement(
+        MyTitle, 
+        {
+          msg: this.props.msg
+        });
+    } else {
+      return Feact.createElement('h1', null, this.props.msg);
+    }
   }
 });
 
 Feact.render(
-  Feact.createElement(MyTitle, { msg: 'hey there Feact'}),
+  Feact.createElement(MyTitle, { asTitle: true, msg: 'hey there Feact'}),
   document.getElementById('root')
 );
